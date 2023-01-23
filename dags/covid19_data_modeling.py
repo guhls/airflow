5b17aeaf22ce0240e4d0f76b8d0952712c4a6802
Gustavo Silva <guhenriquels@gmail.com>
2023-01-23 10:19:27 -0300

    Tasks grouped with the DAG

--- dags/covid19_data_modeling.py
+++ dags/covid19_data_modeling.py
@@ -1,10 +1,78 @@
+# Imports
 import datetime as dt
+import os
 
+import pandas as pd
+import pyathena
 from airflow.models import DAG
 from airflow.operators.python import PythonOperator
+from dotenv import load_dotenv
+from googleapiclient.discovery import build
 
-from tasks.common.funcs import extract_data, upload_data
-from tasks.covid19_data_modeling.funcs import process_data
+from auth.google.creds import get_creds
+
+load_dotenv()
+
+# Functions
+
+
+def get_data(query):
+    conn = pyathena.connect(
+        s3_staging_dir=os.environ.get('S3_STAGING_DIR'),
+        region_name=os.environ.get('REGION_NAME')
+        )
+
+    return pd.read_sql_query(query, conn)
+
+
+# Tasks functions
+
+def extract_data(*args, **kwargs):
+    query = str(kwargs["query"])
+
+    df = get_data(query)
+
+    return df.to_json(orient='index', date_format='iso')
+
+
+def process_data(*args, **kwargs):
+    data = kwargs['task_instance'].xcom_pull(task_ids='extract_data_task')
+    df = pd.read_json(data)
+    # Data Transform
+    ...
+    return df.to_json(orient='index', date_format='iso')
+
+
+def upload_data(*args, **kwargs):
+    data = kwargs["task_instance"].xcom_pull(task_ids="process_data_task")
+    sheet_id = str(kwargs['sheet_id'])
+    range_ = str(kwargs['range'])
+
+    df = pd.read_json(data)
+
+    service_sheets = build(
+        "sheets", "v4", credentials=get_creds()
+    )
+
+    sheet = service_sheets.spreadsheets()
+
+    values = [list(df)] + df.values.tolist()[0:]
+
+    sheet.values().clear(spreadsheetId=sheet_id, range=range_)
+
+    result = (  # noqa
+        sheet.values()
+        .update(
+            spreadsheetId=sheet_id,
+            range=range_,
+            valueInputOption="RAW",
+            body={"values": values},
+        )
+        .execute()
+    )
+
+
+# DAG
 
 default_args = {
     'owner': 'admin',
--- tasks/covid19_data_modeling/funcs.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-def process_data(*args, **kwargs):
-    data = kwargs['task_instance'].xcom_pull(task_ids='extract_data_task')
-    df = pd.read_json(data)
-    # Data Transform
-    ...
-    return df.to_json(orient='index', date_format='iso')
