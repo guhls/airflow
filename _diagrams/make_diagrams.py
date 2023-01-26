48c48d271c20efa4e867b0b1df1bacee1f61b9ee
Gustavo Silva <guhenriquels@gmail.com>
2023-01-26 01:12:58 -0300

    Updated covid19_data_modeling diagram

--- _diagrams/make_diagrams.py
+++ _diagrams/make_diagrams.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from diagrams import Cluster, Diagram, Edge
 from diagrams.aws.analytics import Athena
 from diagrams.custom import Custom
-from diagrams.generic.database import SQL
 from diagrams.onprem.compute import Server
 from diagrams.onprem.database import Postgresql as PostgreSQL
 from diagrams.programming.language import Python
@@ -11,7 +10,7 @@ from diagrams.programming.language import Python
 path_root = Path(__file__).parent
 
 graph_attr = {
-    "bgcolor": "transparent",
+    "bgcolor": "white",
 }
 
 
@@ -23,26 +22,26 @@ def diagram_1():
             direction="LR",
             graph_attr=graph_attr):
 
-        with Cluster("sources"):
-            dbs = Athena("athena")
+        dbs = Athena("view")
 
         with Cluster("DAG"):
             dag = [
                 Python("extract_data"),
+                Python("process_data"),
                 Python("upload_data")]
 
-            with Cluster("process_data"):
-                primary = [SQL("table"), Server("API")]
-                primary[1] - primary[0]
+        with Cluster("get/add data"):
+            primary = [PostgreSQL("check"), Server("cnes")]
 
         sheets = Custom(
             "Google Sheets",
             f"{path_root}/resources/sheets-sheet-svgrepo-com.png")
 
-        dag[0] >> primary[0] >> dag[1]
-        dag[1] >> sheets
+        dag[0] >> dag[1] >> primary
 
-        dbs >> dag[0]
+        dag[1] >> dag[2] >> sheets
+
+        dbs << dag[0]
 
 
 def diagram_2():
@@ -57,4 +56,4 @@ def diagram_2():
 
 
 if __name__ == "__main__":
-    diagram_2()
+    diagram_1()
